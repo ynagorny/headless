aeb9fb1f73c65981757ec226c57bd472947052dc
Yurie Nagorny <ynagorny@bearincorp.com>
2017-05-31 19:29:08 -0400

    Exploratory hack to hide the cursor

--- lib/headless/video/video_recorder.rb
+++ lib/headless/video/video_recorder.rb
@@ -6,19 +6,24 @@ class Headless
     attr_accessor :pid_file_path, :tmp_file_path, :log_file_path
 
     def initialize(display, dimensions, options = {})
-      CliUtil.ensure_application_exists!('ffmpeg', 'Ffmpeg not found on your system. Install it with sudo apt-get install ffmpeg')
-
-      @display = display
-      @dimensions = dimensions
-
-      @pid_file_path = options.fetch(:pid_file_path, "/tmp/.headless_ffmpeg_#{@display}.pid")
-      @tmp_file_path = options.fetch(:tmp_file_path, "/tmp/.headless_ffmpeg_#{@display}.mov")
-      @log_file_path = options.fetch(:log_file_path, "/dev/null")
       @codec = options.fetch(:codec, "qtrle")
       @frame_rate = options.fetch(:frame_rate, 30)
       @provider = options.fetch(:provider, :libav)  # or :ffmpeg
       @extra = options.fetch(:extra, [])
       @extra = [ @extra ] unless @extra.kind_of? Array
+      @hide_cursor = options.fetch(:hide_cursor, true)
+
+      @display = display
+      @dimensions = dimensions
+
+      CliUtil.ensure_application_exists!('ffmpeg', 'Ffmpeg not found on your system. Install it with sudo apt-get install ffmpeg')
+      CliUtil.ensure_application_exists!('unclutter', 'unclutter not found on your system. Install it with sudo apt-get install unclutter') if @hide_cursor
+
+      @tmp_file_path = options.fetch(:tmp_file_path, "/tmp/.headless_ffmpeg_#{@display}.mov")
+      @pid_file_path = options.fetch(:pid_file_path, "/tmp/.headless_ffmpeg_#{@display}.pid")
+      @unclutter_pid_file_path = options.fetch(:unclutter_pid_file_path, "/tmp/.headless_unclutter_#{@display}.pid")
+      @log_file_path = options.fetch(:log_file_path, "/dev/null")
+      @unclutter_log_file_path = options.fetch(:unclutter_log_file_path, "/dev/null")
     end
 
     def capture_running?
@@ -36,6 +41,7 @@ class Headless
 
       extra = @extra.join(' ')
 
+      CliUtil.fork_process("#{CliUtil.path_to('unclutter')} -display :#{@display} -idle 0.01 -root", @unclutter_pid_file_path, @unclutter_log_file_path)
       CliUtil.fork_process("#{CliUtil.path_to('ffmpeg')} -y -r #{@frame_rate} #{group_of_pic_size_option} -s #{dimensions} -f x11grab -i :#{@display} -vcodec #{@codec} #{extra} #{@tmp_file_path}", @pid_file_path, @log_file_path)
       # give the file 10 seconds to create, then quietly give up
       Retryable.retryable(:tries => 100, :sleep => 0.1) do
@@ -50,7 +56,7 @@ class Headless
     end
 
     def stop_and_save(path)
-      CliUtil.kill_process(@pid_file_path, :wait => true)
+      stop
       if File.exists? @tmp_file_path
         begin
           FileUtils.mv(@tmp_file_path, path)
@@ -61,12 +67,19 @@ class Headless
     end
 
     def stop_and_discard
-      CliUtil.kill_process(@pid_file_path, :wait => true)
+      stop
       begin
         FileUtils.rm(@tmp_file_path)
       rescue Errno::ENOENT
         # that's ok if the file doesn't exist
       end
+    end
+
+    private
+
+    def stop
+      CliUtil.kill_process(@unclutter_pid_file_path)
+      CliUtil.kill_process(@pid_file_path, :wait => true)
     end
   end
 end
--- spec/video_recorder_spec.rb
+++ spec/video_recorder_spec.rb
@@ -6,21 +6,37 @@ describe Headless::VideoRecorder do
   end
 
   describe "instantiation" do
-    before do
-      Headless::CliUtil.stub(:application_exists?).and_return(false)
+    context 'no ffmpeg installed' do
+      before do
+        Headless::CliUtil.stub(:application_exists?).and_return(false)
+      end
+
+      it "throws an error if ffmpeg is not installed" do
+          lambda { Headless::VideoRecorder.new(99, "1024x768x32") }.should raise_error(Headless::Exception)
+      end
     end
 
-    it "throws an error if ffmpeg is not installed" do
+    context 'no unclutter installed' do
+      before do
+        Headless::CliUtil.stub(:application_exists?).with('ffmpeg').and_return(true)
+        Headless::CliUtil.stub(:application_exists?).with('unclutter').and_return(false)
+      end
+
+      it "throws an error if unclutter is not installed" do
         lambda { Headless::VideoRecorder.new(99, "1024x768x32") }.should raise_error(Headless::Exception)
+      end
     end
   end
 
   describe "#capture" do
-    it "starts ffmpeg" do
-      Headless::CliUtil.stub(:path_to).and_return('ffmpeg')
+    it "starts ffmpeg and unclutter" do
+      Headless::CliUtil.stub(:path_to).with('ffmpeg').and_return('ffmpeg')
       Headless::CliUtil.should_receive(:fork_process).with(/ffmpeg -y -r 30 -g 600 -s 1024x768x32 -f x11grab -i :99 -vcodec qtrle/, "/tmp/.headless_ffmpeg_99.pid", '/dev/null')
+      Headless::CliUtil.stub(:path_to).with('unclutter').and_return('unclutter')
+      Headless::CliUtil.should_receive(:fork_process).with(/unclutter -display :99 -idle 0.01 -root/, '/tmp/.headless_unclutter_99.pid', '/dev/null')
 
       recorder = Headless::VideoRecorder.new(99, "1024x768x32")
+      File.stub(:exists?).and_return(true)
       recorder.start_capture
     end
 
@@ -29,6 +45,7 @@ describe Headless::VideoRecorder do
       Headless::CliUtil.should_receive(:fork_process).with(/ffmpeg -y -r 30 -g 600 -s 1024x768x32 -f x11grab -i :99 -vcodec libvpx/, "/tmp/.headless_ffmpeg_99.pid", '/dev/null')
 
       recorder = Headless::VideoRecorder.new(99, "1024x768x32", {:codec => 'libvpx'})
+      File.stub(:exists?).and_return(true)
       recorder.start_capture
     end
   end
@@ -37,15 +54,18 @@ describe Headless::VideoRecorder do
     let(:tmpfile) { '/tmp/ci.mov' }
     let(:filename) { '/tmp/test.mov' }
     let(:pidfile) { '/tmp/pid' }
+    let(:unclutter_pidfile) { '/tmp/unclutter_pid' }
 
     subject do
-      recorder = Headless::VideoRecorder.new(99, "1024x768x32", :pid_file_path => pidfile, :tmp_file_path => tmpfile)
+      recorder = Headless::VideoRecorder.new(99, "1024x768x32", :pid_file_path => pidfile, :tmp_file_path => tmpfile, :unclutter_pid_file_path => unclutter_pidfile)
+      File.stub(:exists?).and_return(true)
       recorder.start_capture
       recorder
     end
 
     describe "using #stop_and_save" do
       it "stops video recording and saves file" do
+        Headless::CliUtil.should_receive(:kill_process).with(unclutter_pidfile)
         Headless::CliUtil.should_receive(:kill_process).with(pidfile, :wait => true)
         File.should_receive(:exists?).with(tmpfile).and_return(true)
         FileUtils.should_receive(:mv).with(tmpfile, filename)
@@ -56,6 +76,7 @@ describe Headless::VideoRecorder do
 
     describe "using #stop_and_discard" do
       it "stops video recording and deletes temporary file" do
+        Headless::CliUtil.should_receive(:kill_process).with(unclutter_pidfile)
         Headless::CliUtil.should_receive(:kill_process).with(pidfile, :wait => true)
         FileUtils.should_receive(:rm).with(tmpfile)
 
